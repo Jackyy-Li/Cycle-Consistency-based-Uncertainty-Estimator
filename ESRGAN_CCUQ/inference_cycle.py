373f59688f6d1d6d958c01d2aa821c57cbc0a625
Jianing Li <98203486+Jackyy-Li@users.noreply.github.com>
2023-06-05 15:22:57 -0700

    Change Dependency directory & add demo input images for DeepRFT

--- ESRGAN_CCUQ/inference_cycle.py
+++ ESRGAN_CCUQ/inference_cycle.py
@@ -15,8 +15,8 @@ from basicsr.archs.rrdbnet_arch import RRDBNet
 from basicsr.utils.download_util import load_file_from_url
 from realesrgan import RealESRGANer
 from realesrgan.archs.srvgg_arch import SRVGGNetCompact
-from ESRGAN_CCUQ.Pooling import Cycle
-from DeepRFT_utils import noise, random_crop
+from Pooling import Cycle
+from utils import noise, random_crop
 
 
 def main():
